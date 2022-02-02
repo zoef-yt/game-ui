a917587a92fa7931b4467ce427444a3bed10cc0b
Zoef <shaikhzoef36@gmail.com>
2022-02-02 18:03:48 +0530

    documentation without the card

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 function changeIframe(newSrc) {
 	document.getElementById("showcase-display-iframe").src = newSrc;
 }
+
+document.querySelectorAll(".showcase-list-item").forEach(function (item) {
+	item.addEventListener("click", function () {
+		document.querySelectorAll(".showcase-list-item-active").forEach(function (item) {
+			item.classList.remove("showcase-list-item-active");
+		});
+		item.classList.add("showcase-list-item-active");
+	});
+});
